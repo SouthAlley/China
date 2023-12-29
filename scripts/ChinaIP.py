45182728d90dcf1ef9baf789ebfa93aa46621edd
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-29 18:09:33 +0800

    Update ChinaIP.py

--- scripts/ChinaIP.py
+++ scripts/ChinaIP.py
@@ -1,8 +1,7 @@
-# 导入requests模块
 import requests
 
 # 获取IPv4地址列表
-v4China = "https://raw.githubusercontent.com/SouthAlley/z/main/Surge/cnRule/cnRule.list"
+v4China = "https://raw.githubusercontent.com/pmkol/easymosdns/rules/china_ip_list.txt"
 r = requests.get(v4China)
 
 # 读取原始IPv4地址文件并转换成单独行的IP-CIDR形式
@@ -14,7 +13,19 @@ for ip in original_content:
         ip_cidr = "IP-CIDR," + ip.strip()  # 添加 "IP-CIDR," 前缀
         ip_cidr_content.append(ip_cidr)
 
+# 获取IPv6地址列表
+v6China = "https://raw.githubusercontent.com/pmkol/easymosdns/rules/china_ip_list.txt"
+r_v6 = requests.get(v6China)
+
+# 读取原始IPv6地址文件并转换成单独行的IP-CIDR6形式
+original_content_v6 = r_v6.text.split('\n')
+
+for ip_v6 in original_content_v6:
+    if ip_v6.strip() and ':' in ip_v6:
+        ip_cidr_v6 = "IP-CIDR6," + ip_v6.strip()  # 添加 "IP-CIDR6," 前缀
+        ip_cidr_content.append(ip_cidr_v6)
+
 # 写入单独行的"IP-CIDR,IP"形式的内容到新文件
-with open("IPv4.China.list", "w") as cidr_file:
+with open("IP.China.list", "w") as cidr_file:
     for line in ip_cidr_content:
         cidr_file.write(line + '\n')
