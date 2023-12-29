f9061312509645b7f128095c06676cf7291bd0d5
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-29 18:12:33 +0800

    Update cn.py

--- scripts/cn.py
+++ scripts/cn.py
@@ -1,11 +1,10 @@
-# 导入requests模块
 import requests
 
-# 获取IPv4地址列表
-v4China = "https://ispip.clang.cn/all_cn.txt"
-r = requests.get(v4China)
+# 获取IPv4和IPv6地址列表
+chinaIPList = "https://raw.githubusercontent.com/pmkol/easymosdns/rules/china_ip_list.txt"
+r = requests.get(chinaIPList)
 
-# 读取原始IPv4地址文件并转换成单独行的IP-CIDR形式
+# 读取原始地址文件并转换成单独行的IP-CIDR形式
 original_content = r.text.split('\n')
 ip_cidr_content = []
 
@@ -13,8 +12,11 @@ for ip in original_content:
     if ip.strip() and '.' in ip:
         ip_cidr = "IP-CIDR," + ip.strip()  # 添加 "IP-CIDR," 前缀
         ip_cidr_content.append(ip_cidr)
+    elif ip.strip() and ':' in ip:
+        ip_cidr_v6 = "IP-CIDR6," + ip.strip()  # 添加 "IP-CIDR6," 前缀
+        ip_cidr_content.append(ip_cidr_v6)
 
 # 写入单独行的"IP-CIDR,IP"形式的内容到新文件
-with open("China.list", "w") as cidr_file:
+with open("IP.China.list", "w") as cidr_file:
     for line in ip_cidr_content:
         cidr_file.write(line + '\n')
